588d8d8d9ea673c561df4354c60b22f6a79b0aaa
Rakshita <rakshitaswami12@gmail.com>
2025-08-06 18:37:12 +0530

    Update: modified

---
+++ src/component/hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+       <section className="text-center py-16 px-4">
+        <h2 className="text-5xl font-light">Healthcare Transformation Solutions</h2>
+        <p className="mt-4 text-dark max-w-2xl mx-auto">
+          Transforming healthcare with AI, XR, and Digital Learning to solve workforce gaps, control cost, & boost patient engagement through iimmersive, intelligent, & scalable solutions.
+        </p>
+        <h1 className="mt-4 max-w-2xl mx-auto text-xl font-medium">Explore the Transformation Landscape</h1>
+      </section>
+  )
+}
+
+export default Hero
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,11 @@ module.exports = {
   theme: {
     extend: {},
   },
+  safelist: [
+    'bg-gradient-to-r',
+    'from-[rgb(105,189,242)]',
+    'via-[rgb(212,128,242)]',
+    'to-[rgb(255,191,128)]',
+  ],
   plugins: [],
-}
+};
